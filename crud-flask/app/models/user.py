7274d87ffb0ce0970563528747e0211f09686e21
Gustitreshana <gustith2105@gmail.com>
2024-03-08 23:25:25 +0700

    Refactor User model in crud-flask app

--- crud-flask/app/models/user.py
+++ crud-flask/app/models/user.py
@@ -1,24 +1,22 @@
 from app.models.base import Base
 from sqlalchemy import Integer, String, DateTime
-from sqlalchemy.orm import mapped_column
 from sqlalchemy.sql import func
 from flask_login import UserMixin
-
 import bcrypt
-
+from sqlalchemy.orm import mapped_column
 
 class User(Base, UserMixin):
     __tablename__ = 'users'
-    
-    id = mapped_column(Integer, primary_key=True)
+
+    id = mapped_column(Integer, primary_key=True, autoincrement=True)
     username = mapped_column(String(100), nullable=False, unique=True)
     email = mapped_column(String(100), nullable=False, unique=True)
     password = mapped_column(String(100), nullable=False)
     created_at = mapped_column(DateTime(timezone=True), server_default=func.now())
-    
+
     def __repr__(self):
-        return f'<User {self.username}>'
-    
+        return f'<User id={self.id}, username={self.username}, email={self.email}>'
+
     def set_password(self, password):
         self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
         
